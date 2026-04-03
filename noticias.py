9864ac85c392cebd75caeaf70b67711c2fc6de56
isaachenry19 <isaachenry1905@gmail.com>
2026-04-02 20:48:59 -0500

    Update noticias.py

--- noticias.py
+++ noticias.py
@@ -1,31 +1,28 @@
 import requests
 import os
 import feedparser
-from datetime import datetime
 
 topic = os.environ.get("NTFY_TOPIC", "noticias-isaac")
 
-# Detectar que notificacion mandar segun la hora
-hora = datetime.utcnow().hour
+# Noticias globales - BBC
+feed_global = feedparser.parse("http://feeds.bbci.co.uk/news/world/rss.xml")
+noticias_global = "\n".join([f"- {n.title}" for n in feed_global.entries[:3]])
 
-if hora == 14:  # 9am Panama = 14 UTC
-    # NOTICIAS GLOBALES
-    feed = feedparser.parse("http://feeds.bbci.co.uk/news/world/rss.xml")
-    titulo_notif = "Isaac! Aqui tienes las noticias globales del dia"
-    noticias = feed.entries[:3]
-else:
-    # NOTICIAS PANAMA
-    feed = feedparser.parse("https://www.laestrella.com.pa/feed")
-    titulo_notif = "Isaac! Lo que esta pasando en Panama ahora"
-    noticias = feed.entries[:3]
+# Noticias Panama - TVN
+feed_panama = feedparser.parse("https://www.tvn-2.com/rss/")
+noticias_panama = "\n".join([f"- {n.title}" for n in feed_panama.entries[:3]])
 
-mensaje = "\n\n".join([f"- {n.title}" for n in noticias])
+mensaje = f"""MUNDO:
+{noticias_global}
+
+PANAMA:
+{noticias_panama}"""
 
 requests.post(
     f"https://ntfy.sh/{topic}",
     data=mensaje.encode("utf-8"),
     headers={
-        "Title": titulo_notif,
+        "Title": "Isaac! Aqui tienes las noticias del dia",
         "Priority": "default",
         "Tags": "newspaper"
     }
